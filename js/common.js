32a1d7cc6334c9c8cc9ed2700460cda3e75c061e
phplpsummer <sh052100@naver.com>
2021-11-16 03:48:24 +0900

    1차 솔로 작업완성

---
+++ js/common.js
@@ -0,0 +1,58 @@
+/* header ---------------------------------------------- */
+const $btnMenuMo = $(".btn_menu_mo");
+let isDone = true;
+
+$("#gnb>li").on("mouseenter",function(){
+    $(this).find(".subMenu").show();
+    $(this).find(".subMenu").animate({opacity:1},250);
+});
+$("#gnb>li").on("mouseleave",function(){
+    $(this).find(".subMenu").hide();
+    $(this).find(".subMenu").animate({opacity:0},250);
+});
+
+//tab키 포커스 연결
+$("#gnb>li").each(function(index){
+    $("#gnb>li").eq(index).find("a").on("focusIn",function(){
+        $("#gnb>li").eq(index).find(".subMenu").show();
+    });
+    $("#gnb>li").eq(index).find("a").on("focusOut",function(){
+        $("#gnb>li").eq(index).find(".subMenu").hide();
+    });
+});
+
+//모바일버전 menu햄버거 버튼
+$btnMenuMo.on("click",function(e){
+    e.preventDefault();
+
+    let isOn = $(this).hasClass("on");
+    if(isOn) {
+        $(this).removeClass("on");
+        $("#gnb_mo").removeClass("on");
+    } else {
+        $(this).addClass("on");
+        $("#gnb_mo").addClass("on");
+    }
+});
+
+$("#gnb_mo >li .plus").on("click",function(){
+    let isOn = $(this).hasClass("on");
+    if(isOn){
+        $(this).removeClass("on");
+        $(this).parent().find(".sub_mo").slideUp(500);
+    } else {
+        $(this).addClass("on");
+        $(this).parent().find(".sub_mo").slideDown(500);
+    }
+});
+
+/* footer---------------------------------------------- */
+const $btnTop = $(".go_top");
+
+$btnTop.on("click",function(e){
+    e.preventDefault();
+
+    $("html, body").animate({
+        scrollTop : 0
+    },500);
+});
--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const $bg = $("#visual main .bg");
 const $main = $("#visual main .mainImg");
 const $smallMain = $("#visual main .smallMain");
 const $largeMain = $("#visual main .largeMain");
+const $title = $("#visual main .title")
 let num = 1;
 let isDone = true;
 
@@ -10,31 +11,50 @@ $(".btnNext").on("click",function(){
     if(isDone) {
         isDone = false;
 
-        $bg.children("li").fadeOut(500);
-        $bg.children("li").first().delay(500).appendTo($bg);
-        $bg.children("li").eq(1).delay(3500).fadeIn(500,function(){
-            isDone=true;
-        });
-    
-        $largeMain.children("li").removeClass("on");
+        if(num==2){
+            num=-1;
+        }
+
+        bgNext();
+        mainNext();
+
+        $title.children("li.on").addClass("upper");
         setTimeout(function(){
-            $smallMain.children("li").removeClass("on");
-        }, 500);
-    
+            $title.children("li").removeClass("upper");
+            $title.children("li").removeClass("on");
+        }, 1000);
         setTimeout(function(){
-            if(num<2) {
-                $smallMain.children("li").eq(num+1).delay(1000).addClass("on");
-                $largeMain.children("li").eq(num+1).delay(1000).addClass("on");
-                num++;
-            } else {
-                num=0;
-                $smallMain.children("li").eq(num).delay(1000).addClass("on");
-                $largeMain.children("li").eq(num).delay(1000).addClass("on");
-            }
-        }, 3000);
+            $title.children("li").eq(num).addClass("on");
+        }, 2000);
+
+        num++;
     }
-
-
-
-
 });
+
+function bgNext(){
+    $bg.children("li").fadeOut(500);
+    $bg.children("li").first().delay(500).appendTo($bg);
+    $bg.children("li").eq(1).delay(2750).fadeIn(500,function(){
+        isDone=true;
+    });
+}
+
+function bgPrev(){
+    $bg.children("li").fadeOut(500);
+    $bg.children("li").last().delay(500).prependTo($bg);
+    $bg.children("li").eq(1).delay(2750).fadeIn(500,function(){
+        isDone=true;
+    });
+}
+
+function mainNext(){
+    $largeMain.children("li").removeClass("on");
+    setTimeout(function(){
+        $smallMain.children("li").removeClass("on");
+    }, 500);
+
+    setTimeout(function(){
+        $smallMain.children("li").eq(num).addClass("on");
+        $largeMain.children("li").eq(num).addClass("on");
+    }, 2000);
+}
