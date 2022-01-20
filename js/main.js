31799a01bdb07c4ccd69ec92c6b06072fe93c547
phplpsummer <sh052100@naver.com>
2022-01-20 18:09:40 +0900

    인터렉션(기본) 완료

---
+++ js/main.js
@@ -0,0 +1,58 @@
+const frame = document.querySelector("#visual main");
+const btnPrev = frame.querySelector(".btnPrev");
+const btnNext = frame.querySelector(".btnNext");
+const bg_ul = frame.querySelector(".bg");
+const bgs = bg_ul.querySelectorAll("li");
+const sMain_ul = frame.querySelector(".smallMain");
+const sMainImgs = sMain_ul.querySelectorAll("li");
+const lMain_ul = frame.querySelector(".largeMain");
+const lMainImgs = lMain_ul.querySelectorAll("li");
+const title_ul = frame.querySelector(".txt");
+const titles = title_ul.querySelectorAll("ul");
+let i = 1;
+
+btnNext.addEventListener("click",(e)=>{
+    e.preventDefault();
+
+    (i > 1 ? i=0 : i++);
+
+    //title
+    title_ul.querySelector(".on").classList.add("upper");
+    setTimeout(()=>{
+        titles.forEach((title)=>{
+            title.classList.remove("on");
+            title.classList.remove("upper");
+        });
+    }, 1000);
+    setTimeout(()=>{
+        titles[i].classList.add("on");
+    }, 1500);
+
+    //main img
+    lMain_ul.querySelector(".on").classList.remove("on");
+    setTimeout(()=>{
+        sMain_ul.querySelector(".on").classList.remove("on");
+    }, 500);
+    setTimeout(()=>{
+        sMainImgs[i].classList.add("on");
+    }, 1500);
+    setTimeout(()=>{
+        lMainImgs[i].classList.add("on");
+    }, 1750);
+
+    //bg
+    let j = i - 1;
+    if(j < 0) j = 2;
+    new Anim(bgs[j],{
+        prop: "opacity",
+        value: 0,
+        duration: 500,
+    });
+    setTimeout(()=>{
+        new Anim(bgs[i],{
+            prop: "opacity",
+            value: 1,
+            duration: 500,
+        });
+    }, 1500);
+});
